d1edbcae0361c1c4ab7e569c93528bfb666ca172
namju <namjubae@gmail.com>
2022-08-20 13:09:25 -0500

    signup fixed

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -40,12 +40,14 @@ export const login = (user) => async (dispatch) => {
 
 
 export const signup=(user)=> async (dispatch)=>{
-    const {username, email, password} = user;
+    const {username, firstName, lastName, email, password} = user;
     const response = await csrfFetch("/api/users", {
         method: "POST",
         body: JSON.stringify({
             username,
             email,
+            firstName,
+            lastName,
             password,
         }),
     });
