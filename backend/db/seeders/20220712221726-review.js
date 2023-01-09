a6023c41f783afd0016780b228d7a2ea8ec9bfaf
namju <namjubae@gmail.com>
2023-01-08 21:11:31 -0600

    12 seeding

--- backend/db/seeders/20220712221726-review.js
+++ backend/db/seeders/20220712221726-review.js
@@ -44,6 +44,42 @@ module.exports = {
      review: "This is a great house for a Fall/Winter girls getaway weekend! Great location close to Baraboo, Wisconsin Dells and several hiking trails at Devils' Lake and Parfrey's Glen. The house is" ,
      stars: 5,
    },
+    {
+     userId: 1,
+     spotId: 7,
+     review: "this home is absolutely stunning!! the sand dunes are a short bike ride away!! would definitely recommend and hope to come back!" ,
+     stars: 4,
+   },
+    {
+     userId: 2,
+     spotId: 8,
+     review: "The best type of place to stay and the best type of home owner to work with. Andy was great with communication and was always fast to respond. Above and beyond type of home" ,
+     stars: 5,
+   },
+    {
+     userId: 3,
+     spotId: 9,
+     review: "My boyfriend and I loved loved loved staying at The Sap House. Being within the woods surrounded by nature was the perfect get away from the city." ,
+     stars: 3,
+   },
+    {
+     userId: 1,
+     spotId: 10,
+     review: "My boyfriend and I loved loved loved staying at The Sap House. Being within the woods surrounded by nature was the perfect get away from the city." ,
+     stars: 4,
+   },
+    {
+     userId: 2,
+     spotId: 11,
+     review: "Amazing stay! So peaceful and beautiful, I would 10/10 go back! Highly recommend RC’s tacos too— great authentic Mexican food." ,
+     stars: 5,
+   },
+    {
+     userId: 3,
+     spotId: 12,
+     review: "Absolutely enjoyed my stay! The entire place was very clean and check in was easy. Everything was as described. I would definitely stay again!" ,
+     stars: 5,
+   },
   //  
   ], {});
   },
--- backend/db/seeders/20220727225445-image.js
+++ backend/db/seeders/20220727225445-image.js
@@ -231,7 +231,228 @@ module.exports = {
         reviewId: 6,
         userId:1
       },
+
+
+      {
+        url: "https://a0.muscache.com/im/pictures/6ba23e66-e847-46d3-b372-c861fc31b90d.jpg?im_w=960",
+        previewImage: true,
+        spotId: 7,
+        reviewId: 7,
+        userId:2
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/51ba83ef-d9f5-4862-9628-8dcd8b2146c6.jpg?im_w=720",
+        previewImage: false,
+        spotId: 7,
+        reviewId: 7,
+        userId:2
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/906dc848-7b82-49e9-8665-0c3fd27778e6.jpg?im_w=720",
+        previewImage: false,
+        spotId: 7,
+        reviewId: 7,
+        userId:2
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/155eced6-62dc-4192-8f17-9f4c57db3ab2.jpg?im_w=720",
+        previewImage: false,
+        spotId: 7,
+        reviewId: 7,
+        userId:2
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/9f3efb2b-ab70-42ee-b2c6-09255cf0003f.jpg?im_w=720",
+        previewImage: false,
+        spotId: 7,
+        reviewId: 7,
+        userId:2
+      },
+
+
+      {
+        url: "https://a0.muscache.com/im/pictures/50aaa8d3-899a-4586-9597-b2141563504e.jpg?im_w=960",
+        previewImage: true,
+        spotId: 8,
+        reviewId: 8,
+        userId:3
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/d5de8275-5abb-43a5-8997-0d8dceadfc1c.jpg?im_w=720",
+        previewImage: false,
+        spotId: 8,
+        reviewId: 8,
+        userId:3
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/420e2e17-9a96-4083-96a2-f3c190a450a5.jpg?im_w=720",
+        previewImage: false,
+        spotId: 8,
+        reviewId: 8,
+        userId:3
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/da3f77aa-aae6-45e7-8493-befa63c129aa.jpg?im_w=720",
+        previewImage: false,
+        spotId: 8,
+        reviewId: 8,
+        userId:3
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/cf44aeac-4c94-447d-82f1-47b3962bab57.jpg?im_w=720",
+        previewImage: false,
+        spotId: 8,
+        reviewId: 8,
+        userId:3
+      },
+
+
+      {
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-600149711203105670/original/be2efa0d-37e5-40f9-af92-864ddd2f79ef.jpeg?im_w=960",
+        previewImage: true,
+        spotId: 9,
+        reviewId: 9,
+        userId:1
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-600149711203105670/original/7ea8706f-191d-44e0-b26d-0d7f13918b2d.jpeg?im_w=720",
+        previewImage: false,
+        spotId: 9,
+        reviewId: 9,
+        userId:1
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-600149711203105670/original/62f62532-b9e0-468d-be6a-1c8032f09d34.jpeg?im_w=720",
+        previewImage: false,
+        spotId: 9,
+        reviewId: 9,
+        userId:1
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/3c9d338c-aeca-4a46-ac41-a64a9f84c32e.jpg?im_w=720",
+        previewImage: false,
+        spotId: 9,
+        reviewId: 9,
+        userId:1
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-600149711203105670/original/24baeb4a-8db9-4605-b991-2262ffb741a8.jpeg?im_w=720",
+        previewImage: false,
+        spotId: 9,
+        reviewId: 9,
+        userId:1
+      },
+
+
+      {
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-795795914729528839/original/1d73e6b0-e8ee-4462-a305-a76bfab3d133.jpeg?im_w=960",
+        previewImage: true,
+        spotId: 10,
+        reviewId: 10,
+        userId:2
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-795795914729528839/original/39109ba9-ea30-46d9-abad-752b6ad90bed.jpeg?im_w=720",
+        previewImage: false,
+        spotId: 10,
+        reviewId: 10,
+        userId:2
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-795795914729528839/original/a4500716-a84e-4ad4-b894-b3dbc9853713.jpeg?im_w=720",
+        previewImage: false,
+        spotId: 10,
+        reviewId: 10,
+        userId:2
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-795795914729528839/original/d9fb35ca-b017-4c21-bd43-ed67f64c5547.jpeg?im_w=720",
+        previewImage: false,
+        spotId: 10,
+        reviewId: 10,
+        userId:2
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/miso/Hosting-795795914729528839/original/9e8a1f31-6d9e-4044-a083-24c1f2569c22.jpeg?im_w=720",
+        previewImage: false,
+        spotId: 10,
+        reviewId: 10,
+        userId:2
+      },
           
+
+      {
+        url: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-50568903/original/6ff64c0e-cb05-47be-a0f8-21d1b66fb074.jpeg?im_w=960",
+        previewImage: true,
+        spotId: 11,
+        reviewId: 11,
+        userId:3
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-50568903/original/981811cf-6a52-4b7b-a643-f559e5358982.jpeg?im_w=720",
+        previewImage: false,
+        spotId: 11,
+        reviewId: 11,
+        userId:3
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-50568903/original/12fb990a-b6c6-48e9-865f-7c9479a30fc3.jpeg?im_w=720",
+        previewImage: false,
+        spotId: 11,
+        reviewId: 11,
+        userId:3
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-50568903/original/334fc15f-8843-4011-a2a3-c0ff1456a731.jpeg?im_w=720",
+        previewImage: false,
+        spotId: 11,
+        reviewId: 11,
+        userId:3
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-50568903/original/5588ded9-4975-48d2-ae7a-41dec70e5ff1.jpeg?im_w=720",
+        previewImage: false,
+        spotId: 11,
+        reviewId: 11,
+        userId:3
+      },
+
+
+      {
+        url: "https://a0.muscache.com/im/pictures/ed3a0ef9-7e8d-4b87-bf10-f44a58f34669.jpg?im_w=960",
+        previewImage: true,
+        spotId: 12,
+        reviewId: 12,
+        userId:1
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/206f4d53-bf1b-4ae1-9c9b-6b029e1a97b9.jpg?im_w=720",
+        previewImage: false,
+        spotId: 12,
+        reviewId: 12,
+        userId:1
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/0a18f028-4c77-44ae-9fe8-5f3a74ab36ce.jpg?im_w=720",
+        previewImage: false,
+        spotId: 12,
+        reviewId: 12,
+        userId:1
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/3fa61594-e2b2-44aa-97f2-4e2be43389e2.jpg?im_w=720",
+        previewImage: false,
+        spotId: 12,
+        reviewId: 12,
+        userId:1
+      },
+      {
+        url: "https://a0.muscache.com/im/pictures/22a8f536-306e-4982-a2fe-b7e6a7de6442.jpg?im_w=720",
+        previewImage: false,
+        spotId: 12,
+        reviewId: 12,
+        userId:1
+      },
 
     ], {});
   },
