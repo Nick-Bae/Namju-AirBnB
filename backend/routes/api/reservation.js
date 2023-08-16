ab497c39ea7a0d6cdb26e5756ff8253c0690b4c7
namju <namjubae@gmail.com>
2023-08-16 09:37:05 -0500

    reservation backend delete user

--- backend/routes/api/reservation.js
+++ backend/routes/api/reservation.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
-const { User, Spot, Booking, Review, sequelize, Image } = require('../../db/models');
+const { Spot, Booking, Review, sequelize, Image } = require('../../db/models');
 
 const router = express.Router();
 const { Op } = require("sequelize")
